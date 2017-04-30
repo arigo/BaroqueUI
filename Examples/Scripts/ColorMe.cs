c2ee4ab5d0fbefd344d4aff8c621f78a7d483466
Armin Rigo <arigo@tunes.org>
2017-04-30 19:52:10 +0200

    Check in the demo scene

---
+++ Examples/Scripts/ColorMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaroqueUI;
+
+
+public class ColorMe : MonoBehaviour
+{
+    public string sceneActionName = "Default";
+
+    private void Start()
+    {
+        SceneAction.Register(sceneActionName, gameObject, buttonDown: OnButtonDown);
+    }
+
+    private void OnButtonDown(ControllerAction action, ControllerSnapshot snapshot)
+    {
+        Material mat = GetComponent<Renderer>().material;
+
+        var menu = new Menu {
+            { "Red", () => mat.color = Color.red},
+            { "Green", () => mat.color = Color.green},
+            { "Blue", () => mat.color = Color.blue},
+            { "White", () => mat.color = Color.white},
+        };
+        menu.ShowPopup(action, snapshot);
+    }
+}
---
+++ Examples/Scripts/RotateMe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaroqueUI;
+
+
+public class RotateMe : MonoBehaviour
+{
+    public string sceneActionName = "Default";
+    public BaroqueUI_Dialog dialogSetAngle;
+
+    private void Start()
+    {
+        SceneAction.Register(sceneActionName, gameObject, buttonDown: OnButtonDown);
+    }
+
+    private void OnButtonDown(ControllerAction action, ControllerSnapshot snapshot)
+    {
+        var popup = new Popup(dialogSetAngle);
+        popup.Set("Slider", transform.rotation.eulerAngles.y, onChange: value =>
+        {
+            Vector3 v = transform.rotation.eulerAngles;
+            v.y = value;
+            transform.rotation = Quaternion.Euler(v);
+        });
+        popup.ShowPopup(action, snapshot);
+    }
+
+    public void HandleRotation(float angle)
+    {
+        Vector3 v = transform.rotation.eulerAngles;
+        v.y = angle;
+        transform.rotation = Quaternion.Euler(v);
+    }
+}
